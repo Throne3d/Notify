183b3b402491fc81a4230d5e7160e138df30c248
Edward Jones <throne3d@gmail.com>
2018-02-20 15:18:05 +0000

    Add basic input-and-display (example) functionality

--- app/src/main/java/io/github/throne3d/notify/MainActivity.java
+++ app/src/main/java/io/github/throne3d/notify/MainActivity.java
@@ -1,13 +1,26 @@
 package io.github.throne3d.notify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "io.github.throne3d.notify.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // respond to send message button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
